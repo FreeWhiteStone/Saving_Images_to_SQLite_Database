6876d7807dbdd446aa21abd4911536209048ac8e
Özgür Aktaş <33002420+FreeWhiteStone@users.noreply.github.com>
2018-05-19 00:19:38 +0300

    Update NotificationAlarmService.java

--- app/src/main/java/com/example/free/grad/NotificationAlarmService.java
+++ app/src/main/java/com/example/free/grad/NotificationAlarmService.java
@@ -20,36 +20,12 @@ public class NotificationAlarmService {
     }
 
     public void startAlarm(){
-
-        Database db = new Database(context.getApplicationContext());
-        HashMap<String, String> map2 = db.getProperDate();
-        String date=(map2.get("time_stamp"));
-
-        Log.d("date_from_db",date);
-
-
-        String dateInString = date;  // Start date
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-
         //Set the alarm to 10 seconds from now
         Calendar c = Calendar.getInstance();
-        //Log.d("ceee",c.toString());
         c.add(Calendar.SECOND, 5);
-        //long firstTime = c.getTimeInMillis();
-
-//        Log.d("firstTime",Long.toString(firstTime));
-//        try {
-//            c.setTime(sdf.parse(dateInString));
-//            //Log.d("ceee",c.toString());
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-
         long firstTime = c.getTimeInMillis();
-        Log.d("firstTime1",Long.toString(firstTime));
         // Schedule the alarm!
         AlarmManager am = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         am.set(AlarmManager.RTC_WAKEUP, firstTime, mAlarmSender);
-        Log.d("startAlarm", "start");
     }
 }
